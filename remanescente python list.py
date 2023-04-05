32d924755dce71a327ed024040fd704ae9462b65
Denis <denis_hara@hotmail.com>
2023-04-05 19:07:50 -0300

    py

---
+++ remanescente python list.py
@@ -0,0 +1,128 @@
+
+# frutas_exoticas = ["jaboticaba", "cupuaçu", "graviola"]
+
+# for fruta in frutas_exoticas:
+#     print ("eu adoro", fruta)
+
+# r:eu adoro jaboticaba
+# eu adoro cupuaçu     
+# eu adoro graviola
+
+
+# for i in range(0, 20, 3):
+#     print(i)
+
+# r: 0
+# 3
+# 6
+# 9
+# 12
+# 15
+# 18
+
+# numeros = range(100, 0, -5)
+
+# for x in numeros:
+#     print(x)
+
+# r: 100
+# 95
+# 90
+# 85
+# 80
+# 75
+# 70
+# 65
+# 60
+# 55
+# 50
+# 45
+# 40
+# 35
+# 30
+# 25
+# 20
+# 15
+# 10
+# 5
+# animais = ["gato", "cachorro", "papagaio", "arara", "jacaré"]
+# for x in animais:
+#     print("--> ", x)
+
+# -->  gato
+# -->  cachorro
+# -->  papagaio
+# -->  arara
+# -->  jacaré
+
+# animais = ["gato", "cachorro", "papagaio", "arara", "jacaré"]
+# for x in range(len(animais)):
+#     print("--> ", x)
+
+# r: -->  0
+# -->  1
+# -->  2
+# -->  3
+# -->  4
+
+
+# animais = ["gato", "cachorro", "papagaio", "arara", "jacaré"]
+
+# for x in range(len(animais)):
+#     print("--> ", animais[x])
+
+# -->  gato
+# -->  cachorro
+# -->  papagaio
+# -->  arara
+# -->  jacaré  
+
+
+
+# for i in range(16,4,-3):
+# 	print(i)
+        
+# r:16
+# 13
+# 10
+# 7
+# valores = []
+# for i in range(1, 10):
+#     if i % 2 == 0:
+#         valores.append(i)
+
+# print(valores)
+
+# r:[2, 4, 6, 8]
+# valores = []
+# for i in range(0, 10, 2):
+#     valores.append(i)
+# print(valores)
+
+
+
+
+# animais = ["gato", "cachorro", "papagaio",
+# "arara", "jacaré"]
+# animais[2] = animais.append("piriquito")
+
+# print(animais)
+
+# r = ['gato', 'cachorro', None, 'arara', 'jacaré', 'piriquito']
+
+
+# animais = ["gato", "cachorro", "papagaio",
+# "arara", "jacaré"]
+# animais[-3] = "piriquito"
+
+# print(animais)
+# r = ['gato', 'cachorro', 'piriquito', 'arara', 'jacaré']
+
+
+
+animais = ["gato", "cachorro", "papagaio",
+"arara", "jacaré"]
+
+animais[2] = "piriquito"
+
+print(animais)
