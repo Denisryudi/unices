b45ab1c29e6c621f80d724b6440dc6a4971c4b95
Denis <denis_hara@hotmail.com>
2023-05-28 16:52:40 -0300

    estrutura sequencial

---
+++ jupyter backup/fundamentos.py
@@ -0,0 +1,8 @@
+# 'f' com f-string
+
+faturamento = 1000
+custo = 400
+
+lucro = faturamento - custo
+
+print(f"O faturamento foi de {faturamento} e o lucro foi de {faturamento - custo} ou lucro {lucro}")
