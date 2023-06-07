0c8eae589d2130ff3b4af6870cd6d6eec2aeaa51
Denis <denis_hara@hotmail.com>
2023-06-07 19:42:39 -0300

    python if else elif

--- hashtag_python/fundamentos.py
+++ hashtag_python/fundamentos.py
@@ -6,3 +6,7 @@ custo = 400
 lucro = faturamento - custo
 
 print(f"O faturamento foi de {faturamento} e o lucro foi de {faturamento - custo} ou lucro {lucro}")
+
+print("lalalalalalalalallalala {}".format(lucro))
+
+print(f'lalalalallalalalala {lucro}')
