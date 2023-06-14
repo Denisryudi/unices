dca9370bd7b7fd855736e44d3bddcce747312e43
Denis <denis_hara@hotmail.com>
2023-06-14 20:01:12 -0300

    j

--- java/test.java
+++ java/test.java
@@ -3,3 +3,20 @@ class Programa {
         System.out.println("ola mundo");
       }  
 }
+
+
+public class TestaBool {
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 18;
+		int quantidadePessoas = 1;
+		boolean acompanhado = true;
+		
+		if (idade >= 18 && acompanhado) {
+			System.out.println("seja bem vindo");
+		} else {
+			System.out.println("infelizmente voce nao pode entrar");
+		}
+	}
+}
+
