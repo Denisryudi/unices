e7d58405a721b8903955dffa25b7cd1645c72340
Denis <denis_hara@hotmail.com>
2023-07-15 14:16:22 -0300

    poli

--- java/bytebank-herdado/TesteGerente.java
+++ java/bytebank-herdado/TesteGerente.java
@@ -5,17 +5,19 @@ public class TesteGerente {
 		Gerente oswald = new Gerente();
 		oswald.setNome("Oswald");
 		oswald.setCpf("2321321321");
-		oswald.setSalario(2600.00);
+		oswald.setSalario(5000.00);
 		
 		System.out.println(oswald.getNome());
 		System.out.println(oswald.getCpf());
 		System.out.println(oswald.getSalario());
-		System.out.println(oswald.getBonificacao());
+
 		
 		oswald.setSenha(222222);
 		boolean autenticou = oswald.autentica(55454);
 		
 		System.out.println(autenticou);
+		
+		System.out.println(oswald.getBonificacao());
 	}
 
 }
