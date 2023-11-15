04e128a71c13a3a80884a3a9e507ba30d00662c0
Denis <denis_hara@hotmail.com>
2023-11-15 13:46:34 -0300

    rsrs

---
+++ python/ph.py
@@ -0,0 +1,12 @@
+text = ""
+hexadecimal_result = ''.join([hex(ord(char))[2:] for char in text])
+print(hexadecimal_result)
+
+
+hexadecimal_text = ""
+
+def hex_to_text(hex_string):
+    return bytes.fromhex(hex_string).decode('utf-8')
+
+original_text = hex_to_text(hexadecimal_text)
+print(original_text)
