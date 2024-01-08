11ad6cd032a0f66cca3722a1abdef9ed73fae1a6
Denis <denis_hara@hotmail.com>
2024-01-08 15:45:04 -0300

    for

---
+++ hashtag_python/1if else elif/foreach.py
@@ -0,0 +1,12 @@
+vendas = [1200, 300, 800, 1500, 1900, 2750, 400, 20, 23, 79, 90, 80, 1100, 999, 900, 880, 870, 50, 1111, 120, 300, 450, 800]
+meta = 1000
+
+qtde_bateu_meta = 0
+
+for venda in vendas:
+    if venda >= meta:
+        qtde_bateu_meta += 1
+
+venda_total = len(vendas)
+
+print('O percentual de pessoas que bateram a meta foi de {:.1%}'.format(qtde_bateu_meta/venda_total))
