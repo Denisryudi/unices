87df47d7ed502578410faf2da021e93c4c527e6c
Denis <denis_hara@hotmail.com>
2024-01-11 20:04:39 -0300

    ex w

---
+++ python_hashtag/p4 while/while infi.py
@@ -0,0 +1,11 @@
+#Digamos que temos uma lista de vendedores e as quantidades vendidas e queremos identificar todos os vendedores que bateram a meta de 50 vendas.
+#%%
+vendas = [941, 852, 783, 714, 697, 686, 685, 670, 631, 453, 386, 371, 294, 269, 259, 218, 208, 163, 125, 102, 87, 47, 7]
+vendedores = ['Maria', 'José', 'Antônio', 'João', 'Francisco', 'Ana', 'Luiz', 'Paulo', 'Carlos', 'Manoel', 'Pedro', 'Francisca', 'Marcos', 'Raimundo', 'Sebastião', 'Antônia', 'Marcelo', 'Jorge', 'Márcia', 'Geraldo', 'Adriana', 'Sandra', 'Luis']
+meta = 50
+i = 0
+if vendas[i] >= 50:
+
+        while i <= len(vendas):
+            print(vendas[i], vendedores[i])
+            i += 1
