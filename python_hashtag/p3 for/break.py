b673cdf70cde51f39c25c70a5938d68fa815b629
Denis <denis_hara@hotmail.com>
2024-01-10 22:15:48 -0300

    list

--- python_hashtag/p3 for/break.py
+++ python_hashtag/p3 for/break.py
@@ -7,3 +7,12 @@ for venda in vendas:
         print('Não ganha bonus')
         break
     print(venda)
+
+
+
+for venda in vendas:
+    if venda <= meta:
+        print('Não ganha bonus')
+        continue
+        #pula direto pro próximo - 150. break sai do for, continue pula para o próximo item do for.
+    print(venda)
