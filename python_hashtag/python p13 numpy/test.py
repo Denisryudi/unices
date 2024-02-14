37f23e1ba2cc338e22762c193959681f08b7ee3f
Denis <denis_hara@hotmail.com>
2024-02-14 19:32:20 -0300

    numpy

---
+++ python_hashtag/python p13 numpy/test.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+precos = np.array([25, 20, 35, 30, 40])
+quantidades = np.array([5, 10, 25, 30, 40])
+
+produto_mais_caro = np.max(precos)
+produto_mais_barato = np.min(precos)
+print('produto mais caro = {} e mais barato = {}'.format(produto_mais_caro, produto_mais_barato))
+
+precos_ordenadas = np.sort(precos)
+print(precos_ordenadas)
+
+precos_media = np.mean(precos)
+print(precos_media)
+
+print(quantidades * precos)
+
+print(np.sum(quantidades * precos))
+
+#dot = a soma da quantidades * preços = sum(qtde * precos) == np.dot
+print(np.dot(quantidades, precos))
+
